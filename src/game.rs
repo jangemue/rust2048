29aaa18a738d44b85fed8ea3626310e0ff465b28
Jan Mühlig <jan.muehlig@udo.edu>
2017-05-13 12:39:54 +0200

    Struct game

---
+++ src/game.rs
@@ -0,0 +1,29 @@
+pub enum Command {
+    Up,Down,Left,Right
+}
+
+pub struct Game {
+    length : usize,
+    score : u32,
+    field : Vec<Vec<u32>>
+}
+
+impl Game {
+    pub fn new(length : usize) -> Game {
+        let field = vec![vec![0;length]];
+
+        Game{ length: length, score: 0, field : field}
+    }
+
+    pub fn field(&self) -> &Vec<Vec<u32>> {
+        &self.field
+    }
+
+    pub fn score(&self) -> u32 {
+        self.score
+    }
+
+    pub fn command(&self, command : Command) {
+        
+    }
+}
